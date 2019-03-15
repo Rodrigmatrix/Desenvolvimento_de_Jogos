1f7cdc19312855a8e84c88f7b54c3f325676b305
Rodrigmatrix <rodrigmatrix@hotmail.com>
2019-03-15 15:01:38 -0300

    Aula movimentação

---
+++ Aula 1/Assets/Scripts/movimentacao.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movimentacao : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+            this.gameObject.transform.position += new Vector3(0.2f, 0, 0);
+        }
+        else if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            this.gameObject.transform.position += new Vector3(-0.2f, 0, 0);
+        }
+        else if (Input.GetKey(KeyCode.UpArrow))
+        {
+            this.gameObject.transform.position += new Vector3(0.2f, 0, 0.2f);
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            this.gameObject.transform.position += new Vector3(-0.2f, 0, -0.2f);
+        }
+    }
+}
